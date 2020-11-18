afe82c15e3c5d39f8f5bfd0df23224b0a68c32fb
spidercatnat <bloomjazzer@gmail.com>
2020-11-17 22:05:06 -0800

    Sine studies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 import { Canvas } from "./components";
 
+const PHI = (1 + Math.sqrt(5)) / 2;
+
 class HelloCanvas extends Component {
-  draw({ canvas, ctx, width, height }) {
-    ctx.moveTo(0, 0);
-    ctx.lineTo(width, height);
-    ctx.stroke();
+  draw = ({ canvas, ctx, width, height }) => {
+    this.stage = { canvas, ctx, width, height };
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, width, height)
+    this.woosh(1, 1)
+  }
+
+  tattooIdea1 = () => {
+    for (let i = 1; i <= 13; i++) {
+      this.plotSine(i, i)
+    }
+  }
+
+  woosh = (freq, ampl) => {
+    const { canvas, ctx, width, height } = this.stage;
+    this.plotSine(freq, ampl)
+    this.anim = requestAnimationFrame(() => {
+      if (freq <= 13) {
+        this.woosh(freq + 1, (ampl + 10));
+      } else {
+        return cancelAnimationFrame(this.anim)
+      }
+    })
+  }
+
+  plotSine = (frequency = 12, amplitude = 25) => {
+    const { ctx, width, height } = this.stage;
+    const xspacing = 10;
+    const w = width + 10;
+    const period = width * 5;
+    const dx = (2 * Math.PI / period) * xspacing;
+    let yvalues = new Array(Math.round(w))
+    let theta = 0;
+
+    ctx.beginPath()
+    
+    // For every x value, calculate a y value with sine function
+    for (let i = 0; i < yvalues.length; i++) {
+      yvalues[i] = -Math.sin(theta * (frequency)) * amplitude;
+      theta += dx;
+    }
+
+    for (let x = 0; x < yvalues.length; x++) {
+      let y = height / 2 + yvalues[x]
+      ctx.lineTo(x, y)
+    }
+
+    ctx.stroke()
+    ctx.closePath()
   }
   render() {
     const { dimensions } = this.props;
@@ -13,9 +60,9 @@ class HelloCanvas extends Component {
       <Canvas
         onMount={this.draw}
         onResize={this.draw}
-        refreshRate={10}
+        refreshRate={0}
         dimensions={dimensions}
-        style={{ margin: "0 auto" }}
+        style={{ margin: "0 auto", }}
       />
     )
   }
