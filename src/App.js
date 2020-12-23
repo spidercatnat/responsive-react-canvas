b3f8a48ba9fd134eeec268c1ee789e3f5e2c6b3f
spidercatnat <bloomjazzer@gmail.com>
2020-12-22 21:51:59 -0800

    Adds NN diagram w sliders

--- src/App.js
+++ src/App.js
@@ -1,194 +1,168 @@
 import React, { Component } from "react";
 import { Canvas } from "./components";
+import { Slider } from "antd";
+import 'antd/dist/antd.css';
+import { Note, Scale } from "@tonaljs/tonal";
 
-const TWO_PI = 2 * Math.PI;
-const PHI = (1 + Math.sqrt(5)) / 2;
-
-class SineWave {
-  constructor(
-    frequency,
-    amplitude,
-    period = 2,
-    xspacing = 1,
-    { canvasWidth, canvasHeight, ctx },
-    r = 0,
-    g = 0,
-    b = 0,
-    cos
-  ) {
-    this.cos = cos;
-    this.initAmp = amplitude;
-    this.canvasWidth = canvasWidth;
-    this.canvasHeight = canvasHeight;
-    this.ctx = ctx;
-    this.xspacing = xspacing;
-    this.amplitude = amplitude;
-    this.period = period;
-    this.w = canvasWidth + xspacing;
-    this.dx = TWO_PI / (canvasWidth / period);
-    this.yvalues = new Array(this.w);
+class Neuron {
+  constructor(x, y, r, l) {
+    this.x = x;
+    this.y = y;
     this.r = r;
-    this.g = g;
-    this.b = b;
-    this.theta = 0;
-    this.coords = [];
-    this.increasing = true;
-    this.decreasing = false;
+    this.l = l;
   }
 
-  calcWave = () => {
-    const { yvalues, dx, canvasHeight } = this;
-    const threshold = this.initAmp * 9;
-    if (this.amplitude >= threshold) {
-      this.increasing = false;
-      this.decreasing = true;
-    }
+  draw = (ctx) => {
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
+    ctx.fillStyle = "black";
+    ctx.font = `${this.r}px Helvetica`;
+    ctx.fillText(this.l, this.x, this.y + 3)
+    ctx.closePath()
+  }
 
-    if (this.amplitude <= 0) {
-      this.increasing = true;
-      this.decreasing = false;
-    }
+  connect = (ctx, neuron) => {
+    if (!neuron) return;
+    ctx.beginPath()
+    ctx.moveTo(this.x + this.r, this.y);
+    ctx.lineTo(neuron.x - neuron.r, neuron.y);
+    ctx.stroke();
+    ctx.closePath()
 
-    if (this.increasing) {
-      this.amplitude = this.amplitude + 1;
-    }
-
-    if (this.decreasing) {
-      this.amplitude = this.amplitude - 1;
-    }
-
-    // Increment theta (try different values for 'angular velocity' here
-    this.theta += 0.01;
-    let x = this.theta;
-
-    for (let i = 0; i < yvalues.length; i++) {
-
-
-      if (this.cos) {
-        //scaled amplitudes... (A)
-        // this.yvalues[i] = Math.cos(x) * this.amplitude;
-        // scaled overtones (B)
-        this.yvalues[i] = Math.cos(this.amplitude * x) * (this.amplitude);
-      } else {
-        this.yvalues[i] = Math.sin(x) * this.amplitude;
-        // scaled overtones (B)
-        // this.yvalues[i] = Math.sin(this.amplitude * x) * (this.amplitude);
-      }
-
-      this.coords[i] = {
-        x: i * this.xspacing,
-        y: canvasHeight / 2 + this.yvalues[i],
-      };
-      x += dx;
-    }
-  };
+  }
 }
 
+
 class HelloCanvas extends Component {
+  state = {
+    radix: 13,
+    input: 1
+  }
+
   draw = ({ canvas, ctx, width, height }) => {
+    // Setup
     this.stage = { canvas, ctx, width, height };
     ctx.clearRect(0, 0, width, height);
     ctx.lineCap = "round";
     ctx.lineJoin = "round";
-    ctx.lineWidth = 0.2;
-    this.waves = [];
-    for (let i = 0; i <= 26; i++) {
-      if (this.waves[i]?.anim) cancelAnimationFrame(this.anim);
-      // this.waves.push(
-      //   new SineWave(
-      //     1, // frequency
-      //     i, //amplitude
-      //     1, // period
-      //     1, // xspacing
-      //     {
-      //       canvasWidth: width,
-      //       canvasHeight: height,
-      //       ctx,
-      //     },
+    ctx.textAlign = "center";
+    ctx.textBaseline = 'middle';
+    ctx.lineWidth = 0.25;
 
-      //     168, 218, 220
-      //     , true
-      //   )
-      // );
-      this.waves.push(
-        new SineWave(
-          1, // frequency
-          i, //amplitude
-          i, // period
-          i, // xspacing
-          {
-            canvasWidth: width,
-            canvasHeight: height,
-            ctx,
-          },
-          // 255,255,255
-          168, 218, 220
-          , false
-        )
-      )
-      this.waves.push(
-        new SineWave(
-          1, // frequency
-          i, //amplitude
-          i, // period
-          1, // xspacing
-          {
-            canvasWidth: width,
-            canvasHeight: height,
-            ctx,
-          },
-          // 255,255,255
-          168, 218, 220
-          , false
-        )
-      );
+    // Draw input layer
+    this.inputLayer = this.drawInputLayer([this.state.input])
+
+    // Draw module layer
+    this.layerOne = this.drawModule(Array.from(Array(this.state.radix - 1).keys()).map(k => k + 1));
+
+    // Draw ring layer
+    this.outputLayer = this.drawRing(
+      Scale.rangeOf("C major")("C2", "C5").slice(0, this.state.radix - 1)
+    )
+
+    // Connect the input layer to the module layer
+    for (const n of this.layerOne) {
+      this.inputLayer[0].connect(ctx, n)
     }
 
-    this.renderWaves();
-  };
-
-  renderWaves = () => {
-    const { ctx, width, height } = this.stage;
-    ctx.filleStyle = "black"
-    ctx.fillRect(0, 0, width, height);
-    this.waves.forEach((w) => {
-      const { ctx, width, height } = this.stage;
-      const { r, g, b, coords, calcWave } = w;
-      calcWave();
-
-      ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`;
-
-      ctx.beginPath();
-      coords.forEach((point, index) => {
-        ctx.moveTo(point.x, point.y);
-        if (index < coords.length - 1) {
-          ctx.lineTo(coords[index + 1].x, coords[index + 1].y);
-        }
-        // ctx.arc(point.x, point.y, 0.25, 0, TWO_PI);
-      });
-      ctx.stroke();
-      ctx.closePath();
-      ctx.closePath();
+    // Connect the module layer to the ring layer
+    this.layerOne.forEach((n, i) => {
+      const index = !((n.l * (this.inputLayer[0].l)) % this.state.radix) ? this.state.radix : ((n.l * (this.inputLayer[0].l)) % this.state.radix);
+      n.connect(ctx, this.outputLayer[(index) - 1])
     });
-    requestAnimationFrame(this.renderWaves);
+
   };
+
+  drawInputLayer = (input) => {
+    const { width, height, ctx } = this.stage;
+    const neurons = [];
+    input.forEach((element, index) => {
+      const neuron = new Neuron(25, height / (index + 2), (height / (3 * this.state.radix)), input); // Label is the last param
+      neurons.push(neuron);
+      neuron.draw(ctx)
+    });
+    return neurons;
+  }
+
+  drawModule = (nodes) => {
+    const { width, height, ctx } = this.stage;
+    const neurons = [];
+    nodes.forEach((element, index) => {
+      const neuron = new Neuron(
+        (width / 2) + (height / (3 * nodes.length)),
+        index * (height / nodes.length) + (height / (3 * nodes.length)),
+        (height / (3 * nodes.length)),
+        element)
+      neurons.push(neuron)
+      neuron.draw(ctx)
+    })
+    return neurons;
+  }
+
+  drawRing = (nodes) => {
+    const { width, height, ctx } = this.stage;
+    const neurons = [];
+    for (let i = 0; i < this.state.radix; i++) {
+      // const neuron = new Neuron(
+      //   width - (height / (3 * nodes.length)),
+      //   i * (height / nodes.length) + (height / (3 * nodes.length)),
+      //   (height / (3 * nodes.length)),
+      //   nodes[i])
+      console.log(i * (height / nodes.length) + (height / (3 * nodes.length)))
+      const neuron = new Neuron(
+        width - (height / (3 * nodes.length)), //x
+        i * (height / nodes.length) + (height / (3 * nodes.length)),//y
+        (height / (3 * nodes.length)), //r
+        nodes[i] // label
+      )
+
+      neurons.push(neuron)
+      neuron.draw(ctx)
+    }
+
+    return neurons;
+  }
+
+  handleChangeInput = value => {
+    this.setState({ input: value }, () => this.draw(this.stage))
+  }
+
+  handleChangeRadix = radix => {
+    this.setState({ radix: +radix, input: this.state.input % radix, }, () => this.draw(this.stage))
+
+  }
 
   render() {
     const { dimensions } = this.props;
     return (
-      <Canvas
-        onMount={this.draw}
-        onResize={this.draw}
-        refreshRate={0}
-        dimensions={dimensions}
-        style={{ margin: "0 auto" }}
-      />
+      <div>
+        <h3>Interference Network Graph</h3>
+        <Canvas
+          onMount={this.draw}
+          onResize={this.draw}
+          refreshRate={0}
+          dimensions={dimensions}
+          style={{
+            margin: "0 auto"
+          }}
+        />
+        <div style={{ width: "50vw", margin: "0 auto", marginTop: "3vh" }}>
+          <h4>Radix</h4>
+          <Slider value={this.state.radix} min={5} max={103} onChange={this.handleChangeRadix} />
+          <h4>Input</h4>
+          <Slider value={this.state.input} min={1} max={this.state.radix - 1} onChange={this.handleChangeInput} />
+        </div>
+      </div>
     );
   }
 }
 
 function App() {
-  return <HelloCanvas dimensions={{ width: "100vw", height: "100vh" }} />;
+  return <HelloCanvas dimensions={{ width: "80vw", height: "80vh" }} />;
 }
 
 export default App;
